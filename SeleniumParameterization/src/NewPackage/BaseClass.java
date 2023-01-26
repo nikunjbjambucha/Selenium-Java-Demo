3109ec0e0ad88f1589d64923c0844e3e30504596
nikunjbjambucha <nikunjbjambucha@gmail.com>
2023-01-26 17:01:22 +0530

    Create BaseClass.java

---
+++ SeleniumParameterization/src/NewPackage/BaseClass.java
@@ -0,0 +1,10 @@
+package NewPackage;
+
+import org.openqa.selenium.WebDriver;
+
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class BaseClass {
+	WebDriver driver;
+	ExtentTest test;
+}
